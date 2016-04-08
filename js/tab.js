a92f1501c52acfe4a75ec7c8d3e6d01d327ba02f
Strebkova <strebkova.o.a@gmail.com>
2016-04-08 18:45:45 +0300

    Replased jquery code with js code

---
+++ js/tab.js
@@ -0,0 +1,24 @@
+window.onload=function() {
+
+var container = document.getElementById("tabContainer");
+var navitem = container.querySelector("#tabs__caption ul li");
+var ident = navitem.id.split("_")[1];
+navitem.parentNode.setAttribute("data-current",ident);
+navitem.setAttribute("class","active");
+document.getElementById("tabs__content" + ident).setAttribute("class", "active");
+ var tabs = container.querySelectorAll("#tabs__caption ul li");
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].onclick=displayPage;
+    }
+}
+
+function displayPage() {
+  var current = this.parentNode.getAttribute("data-current");
+  document.getElementById("tabHeader_" + current).removeAttribute("class");
+  document.getElementById("tabs__content" + current).removeAttribute("class");
+
+  var ident = this.id.split("_")[1];
+  this.setAttribute("class","active");
+  document.getElementById("tabs__content" + ident).setAttribute("class", "active");
+  this.parentNode.setAttribute("data-current",ident);
+}
